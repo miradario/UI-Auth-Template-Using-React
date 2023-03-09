dae0f812262db7d31ee2f885c67a0429456e42f5
miradario <miradario@gmail.com>
2023-03-09 10:36:52 -0300

    main

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,60 +1,89 @@
-import React from 'react'
-import { Navbar, Nav, NavDropdown, Button } from 'react-bootstrap';
+import React from "react";
+import { Navbar, Nav, NavDropdown, Button, Image } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import * as routes from "../constants/routes";
-import SignOutButton from './SignOut';
-import { auth } from '../firebase/firebase';
-import cardDetails from './CardDetails';
+import SignOutButton from "./SignOut";
+import { auth } from "../firebase/firebase";
+import cardDetails from "./CardDetails";
 
 function Navigation() {
-    return (
-        <div style={{position: "fixed", top: "0", width: "100%", zIndex: "99"}}>
-          <Navbar className="header" collapseOnSelect expand="lg" bg="primary" variant="dark">
-            <Navbar.Brand><Link to={routes.LANDING}><Button id="mylogo">Brand Logo</Button></Link></Navbar.Brand>
-              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-              <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="mr-auto">
-                  <NavDropdown title={<Button>Features</Button>} id="collasible-nav-dropdown">
-                    {
-                      cardDetails.map(oneCard => <NavDropdown.Item>
-                        <Link to={'/card/' + oneCard.url} style={{ color: 'black', textDecoration: 'none' }}>
-                          {oneCard.title}
-                        </Link>
-                      </NavDropdown.Item> )
-                    }
-                  </NavDropdown>
-                  <Nav.Link href='#ourfooter'>
-                    <Button>Feedback</Button>
-                  </Nav.Link>
-                  <Nav.Link href="https://code-mergers-org.netlify.app/">
-                    <Button>About Us</Button>
-                  </Nav.Link>
-                </Nav>
-                {
-                  auth.currentUser === null ?(
-                  <Nav>
-                    <Nav.Link>
-                      <Link to={routes.SIGN_IN} style={{color: "white"}} >
-                        <Button>SignIn/SignUp</Button>
-                        </Link>
-                    </Nav.Link>
-                  </Nav>):
-                  (<Nav>
-                    <Nav.Link>
-                      <Link to={routes.ACCOUNT} style={{color: "white"}}>
-                        <Button>Profile</Button>
-                      </Link>
-                    </Nav.Link>
-                    <Nav.Link>
-                      <SignOutButton />
-                    </Nav.Link>
-                  </Nav>
-                  ) 
-                }                
-              </Navbar.Collapse>
-          </Navbar>
-        </div>
-    )
+  return (
+    <div
+      style={{
+        position: "fixed",
+        top: "0",
+        width: "100%",
+        zIndex: "99",
+      }}
+    >
+      <Navbar className="header" collapseOnSelect expand="lg" variant="dark">
+        <Navbar.Brand>
+          <Link to={routes.LANDING}>
+            <img src="./guru.png" alt="Branda Logo" height={"70px"} />
+          </Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            {/*  <NavDropdown
+              title={<Button>Features</Button>}
+              id="collasible-nav-dropdown"
+            >
+              {cardDetails.map((oneCard) => (
+                <NavDropdown.Item>
+                  <Link
+                    to={"/card/" + oneCard.url}
+                    style={{ color: "black", textDecoration: "none" }}
+                  >
+                    {oneCard.title}
+                  </Link>
+                </NavDropdown.Item>
+              ))}
+            </NavDropdown> */}
+            {/*  <Nav.Link to={routes.ACCOUNT}>
+              <Button>Users</Button>
+            </Nav.Link> */}
+            {/* <Nav.Link href="https://code-mergers-org.netlify.app/">
+              <Button>About Us</Button>
+            </Nav.Link> */}
+          </Nav>
+          {auth.currentUser === null ? (
+            <Nav>
+              <Nav.Link>
+                <Link to={routes.SIGN_IN} style={{ color: "white" }}>
+                  <Button style={{ color: "white", backgroundColor: "gray" }}>
+                    SignIn/SignUp
+                  </Button>
+                </Link>
+              </Nav.Link>
+            </Nav>
+          ) : (
+            <Nav>
+              <Nav.Link>
+                <Link
+                  to={routes.USERS}
+                  style={{ backgroundColor: ", color: "white" }}
+                >
+                  <Button
+                    style={{
+                      backgroundColor: "white",
+
+                      borderColor: "gray",
+                    }}
+                  >
+                    Users
+                  </Button>
+                </Link>
+              </Nav.Link>
+              <Nav.Link>
+                <SignOutButton />
+              </Nav.Link>
+            </Nav>
+          )}
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
 }
 
 export default Navigation;
