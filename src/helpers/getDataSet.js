065ea90371553d472b6b62da2fd4510520e6eb52
miradario <miradario@gmail.com>
2023-07-13 13:11:31 -0300

    order_filters

--- src/helpers/getDataSet.js
+++ src/helpers/getDataSet.js
@@ -3,9 +3,26 @@ export const getDataSet = (array, attribute) => {
   array.forEach(el => {
     if (el[1][attribute]) set.add(el[1][attribute]?.toLowerCase().trim())
   })
-  const arr = [...set]
+  const arr = orderArray([...set])
   const finalArray = arr.map(el => {
     return { option: el, select: false }
   })
   return finalArray
+}
+
+const orderArray = array => {
+  let min, aux
+  for (let i = 0; i < array.length - 1; i++) {
+    min = i
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[min]) min = j
+    }
+
+    if (min != i) {
+      aux = array[i]
+      array[i] = array[min]
+      array[min] = aux
+    }
+  }
+  return array
 }
