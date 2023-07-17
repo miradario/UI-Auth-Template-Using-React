10305f3b21a3c961e889bf91466c4a96602db8d5
miradario <miradario@gmail.com>
2023-07-17 08:50:35 -0300

    auth_time

--- src/components/withAuthorization.js
+++ src/components/withAuthorization.js
@@ -1,21 +1,22 @@
-import React from "react";
-import { withRouter } from "react-router-dom";
-import AuthUserContext from "./AuthUserContext";
-import { firebase } from "../firebase";
-import * as routes from "../constants/routes";
+import React from 'react'
+import { withRouter } from 'react-router-dom'
+import AuthUserContext from './AuthUserContext'
+import { firebase } from '../firebase'
+import * as routes from '../constants/routes'
 
 const withAuthorization = authCondition => Component => {
   class WithAuthorization extends React.Component {
-    componentDidMount() {
+    componentDidMount () {
       firebase.auth.onAuthStateChanged(authUser => {
         if (!authCondition(authUser)) {
           //if the authorization fails, redirects to sign in page
-          this.props.history.push(routes.SIGN_IN);
+          localStorage.removeItem('email')
+          this.props.history.push(routes.SIGN_IN)
         }
-      });
+      })
     }
 
-    render() {
+    render () {
       return (
         <AuthUserContext.Consumer>
           {/* it either renders the passed component or not */}
@@ -25,11 +26,11 @@ const withAuthorization = authCondition => Component => {
             ) : null
           }
         </AuthUserContext.Consumer>
-      );
+      )
     }
   }
 
-  return withRouter(WithAuthorization); //using withRouter so we have access to history props
-};
+  return withRouter(WithAuthorization) //using withRouter so we have access to history props
+}
 
-export default withAuthorization;
+export default withAuthorization
